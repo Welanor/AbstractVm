b961b77d439382199ea8a7e40fd927767b72dfe1
dellam_a <adrien.dellamaggiora@epitech.eu>
2014-02-13 21:52:01 +0100

    Change the main

--- main.cpp
+++ main.cpp
@@ -2,8 +2,6 @@
 #include "Exception.hpp"
 #include "Operand.hpp"
 
-using namespace std;
-
 int main()
 {
   try
@@ -16,15 +14,33 @@ int main()
       nb2 = createOperand(Int8, "12");
 
       result = *nb1 + *nb2;
-      cout << result->toString() << endl;
+      std::cout << "Result = " << result->toString() << std::endl;
       delete nb1;
       delete nb2;
       delete result;
+
+      nb1 = createOperand(Int32, "10");
+      nb2 = createOperand(Int32, "10");
+
+      result = *nb1 + *nb2;
+      std::cout << "Result = " << result->toString() << std::endl;
+      delete nb1;
+      delete nb2;
+      delete result;
+
+      // nb1 = createOperand(Float, "32.45");
+      // nb2 = createOperand(Float, "12.61");
+
+      // std::cout << "NB1 -> " << nb1->toString() << std::endl;
+      // std::cout << "NB2 -> " << nb2->toString() << std::endl;
+
+      // result = *nb1 % *nb2;
+
       //throw(Exception("toto", "tata"));
     }
   catch (Exception &e)
     {
-      std::cout << "Error " << e.what() << " in " << e.where() << std::endl;
+      std::cerr << "Error: " << e.what() << " in " << e.where() << std::endl;
     }
   return (0);
 }
